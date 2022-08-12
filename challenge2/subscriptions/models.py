da437d4a238a194166b05c5fbb64194c188d19da
olalekan temitayo <olalekantemitayo7@gmail.com>
2022-08-12 09:40:16 +0100

    final cleanup

--- challenge2/subscriptions/models.py
+++ challenge2/subscriptions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 class User(AbstractUser):
@@ -9,6 +10,7 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
+
 
 class Subscription(models.Model):
     stripeSubscriptionId = models.CharField(max_length=255, null=True)
--- challenge2/subscriptions/views.py
+++ challenge2/subscriptions/views.py
@@ -6,12 +6,12 @@ from django.conf import settings
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.http.response import JsonResponse, HttpResponse
-from subscriptions.models import User,Subscription
+from subscriptions.models import User, Subscription
 import json
 from datetime import datetime
 
-
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
 
 # Create your views here.
 @login_required
@@ -29,7 +29,7 @@ def subscriptions_page(request):
                 user.stripeId = stripe_customer.id
                 user.save()
             checkout_session = stripe.checkout.Session.create(
-                client_reference_id=user.id ,
+                client_reference_id=user.id,
                 line_items=[
                     {
                         'price': 'price_1LVe0cEexlX9H8teNNfbBo5H',
@@ -40,7 +40,7 @@ def subscriptions_page(request):
                 subscription_data={
                     'trial_period_days': 7
                 },
-                customer = stripe_customer,
+                customer=stripe_customer,
                 success_url='http://localhost:8000' + reverse('home'),
                 cancel_url='http://localhost:8000' + reverse('cancel'),
             )
@@ -51,7 +51,7 @@ def subscriptions_page(request):
 
     if request.method == 'GET':
         user = request.user
-        if user.subscribed == False:
+        if not user.subscribed:
             return render(request, 'subscribe.html', {'user': request.user})
         else:
             subscription = stripe.Subscription.retrieve(user.user_subscription.stripeSubscriptionId)
@@ -60,7 +60,9 @@ def subscriptions_page(request):
             subscription_ends = datetime.fromtimestamp(subscription.trial_end)
             subscription_status = user.user_subscription.status
             return render(request, 'home.html', {'user': request.user, 'subscription': subscription,
-                                             'product': product, "trial_ends":trial_ends, "subscription_ends":subscription_ends, "subscription_status":subscription_status  })
+                                                 'product': product, "trial_ends": trial_ends,
+                                                 "subscription_ends": subscription_ends,
+                                                 "subscription_status": subscription_status})
 
 
 def success(request):
@@ -71,84 +73,80 @@ def cancel(request):
     return render(request, 'cancel.html')
 
 
-
-#view to cancel current subscription at end cycle.
+# view to cancel current subscription at end cycle.
 @login_required
 def cancel_subscription(request):
-        user = request.user
-        stripe_subscription_id = user.user_subscription.stripeSubscriptionId
-        try:
-            stripe.Subscription.modify(
+    user = request.user
+    stripe_subscription_id = user.user_subscription.stripeSubscriptionId
+    try:
+        stripe.Subscription.modify(
             stripe_subscription_id,
             cancel_at_period_end=True
-            )
-        except Exception as e:
-            return JsonResponse({'error': str(e)})
-        return render(request, 'cancelled.html')
-   
+        )
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
+    return render(request, 'cancelled.html')
 
 
-#webhook that handles event sent from stripe
+# webhook that handles event sent from stripe
 @csrf_exempt
 def stripe_webhook(request):
     # Webhook To securely assign Value of Confirmation of Payment
     stripe.api_key = settings.STRIPE_SECRET_KEY
     payload = request.body
-    event = None
 
     try:
         event = stripe.Event.construct_from(json.loads(payload), stripe.api_key)
-    except ValueError as e:
+    except ValueError:
         # Invalid payload
         return HttpResponse(status=400)
 
     # Handle invoice payment. 
 
-    #we have two scenrio here.
-    #First payment - trial 
-    #subsequent payment 
-
+    # we have two scenario here.
+    # First payment - trial
+    # subsequent payment
 
     if event['type'] == 'invoice.payment_succeeded':
         data = event['data']['object']
-        
+
         # Fetch all the required data from session
         stripe_customer_id = data.get('customer')
         stripe_subscription_id = data.get('subscription')
-        
+
         # Get the user.
         user = User.objects.get(stripeId=stripe_customer_id)
 
-        #recurring payments
+        # recurring payments
         try:
             subscription = Subscription.objects.get(user=user)
             subscription.status = "active"
             subscription.save()
         except Subscription.DoesNotExist:
-            #First_time  subscription has not been made before.
+            # First_time  subscription has not been made before.
             Subscription.objects.create(
                 user=user,
                 status="active",
                 stripeSubscriptionId=stripe_subscription_id,
             )
 
-        #update the subcribed column
+        # update the subscribed column
         user.subscribed = True
         user.save()
         return HttpResponse(status=200)
 
-    #if subcription is deleted after trial ended or user card payment fails
+    # if subscription is deleted after trial ended or user card payment fails
     if event['type'] == 'customer.subscription.deleted':
-        #get info needed
+        # get info needed
         data = event['data']['object']
         stripe_customer_id = data.get('customer')
 
-        #update the current status of subscription to cancelled.
-        user = User.objects.get(stripeId = stripe_customer_id)
+        # update the current status of subscription to cancel.
+        user = User.objects.get(stripeId=stripe_customer_id)
         user.user_subscription.status = "cancelled"
         user.user_subscription.save()
-        
-        #update user
+
+        # update user
         user.subscribed = False
         user.save()
 
@@ -158,12 +156,11 @@ def stripe_webhook(request):
         data = event['data']['object']
         stripe_customer_id = data.get('customer')
 
-        #check if the user want's to cancel
+        # check if the user want's to cancel
         if data.get('cancel_at_period_end'):
-        #update the current status of subscription to cancelled.
-            user = User.objects.get(stripeId = stripe_customer_id)
+            # update the current status of subscription to cancel.
+            user = User.objects.get(stripeId=stripe_customer_id)
             user.user_subscription.status = "scheduled_to_cancel"
             user.user_subscription.save()
             return HttpResponse(status=200)
         return HttpResponse(status=200)
-
